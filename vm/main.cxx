f70d684c6b65309183986473e7ef6de944721338
Felix Bytow <drako@drako.guru>
2024-03-22 01:36:12 +0100

    :recycle: reduced code duplication for keyboard input

--- vm/main.cxx
+++ vm/main.cxx
@@ -119,7 +119,7 @@ int main(int argc, char** argv)
   chip8::CallStack call_stack;
   chip8::Memory memory;
   memory.load(chip8::Processor::CODE_START, content);
-  chip8::Config config = {
+  chip8::Config const config = {
       .register_rw_modifies_i = true,
       .shift_takes_value_from_vy = true,
       .use_vx_for_offset_jump = false,
@@ -153,122 +153,77 @@ int main(int argc, char** argv)
     }
   }};
 
+  auto const toggle_key = [&processor](SDL_Scancode const scancode, bool pressed) {
+    switch (scancode) {
+    default:
+      break;
+    case SDL_SCANCODE_1:
+      processor.toggle_key(0x1, pressed);
+      break;
+    case SDL_SCANCODE_2:
+      processor.toggle_key(0x2, pressed);
+      break;
+    case SDL_SCANCODE_3:
+      processor.toggle_key(0x3, pressed);
+      break;
+    case SDL_SCANCODE_4:
+      processor.toggle_key(0xC, pressed);
+      break;
+    case SDL_SCANCODE_Q:
+      processor.toggle_key(0x4, pressed);
+      break;
+    case SDL_SCANCODE_W:
+      processor.toggle_key(0x5, pressed);
+      break;
+    case SDL_SCANCODE_E:
+      processor.toggle_key(0x6, pressed);
+      break;
+    case SDL_SCANCODE_R:
+      processor.toggle_key(0xD, pressed);
+      break;
+    case SDL_SCANCODE_A:
+      processor.toggle_key(0x7, pressed);
+      break;
+    case SDL_SCANCODE_S:
+      processor.toggle_key(0x8, pressed);
+      break;
+    case SDL_SCANCODE_D:
+      processor.toggle_key(0x9, pressed);
+      break;
+    case SDL_SCANCODE_F:
+      processor.toggle_key(0xE, pressed);
+      break;
+    case SDL_SCANCODE_Z:
+      processor.toggle_key(0xA, pressed);
+      break;
+    case SDL_SCANCODE_X:
+      processor.toggle_key(0x0, pressed);
+      break;
+    case SDL_SCANCODE_C:
+      processor.toggle_key(0xB, pressed);
+      break;
+    case SDL_SCANCODE_V:
+      processor.toggle_key(0xF, pressed);
+      break;
+    }
+  };
+
   while (run) {
     SDL_Event evt;
     while (SDL_PollEvent(&evt)) {
       if (evt.type==SDL_QUIT)
         run = false;
       else if (evt.type==SDL_KEYDOWN) {
-        switch (evt.key.keysym.scancode) {
-        default:
-          break;
-        case SDL_SCANCODE_1:
-          processor.toggle_key(0x1, true);
-          break;
-        case SDL_SCANCODE_2:
-          processor.toggle_key(0x2, true);
-          break;
-        case SDL_SCANCODE_3:
-          processor.toggle_key(0x3, true);
-          break;
-        case SDL_SCANCODE_4:
-          processor.toggle_key(0xC, true);
-          break;
-        case SDL_SCANCODE_Q:
-          processor.toggle_key(0x4, true);
-          break;
-        case SDL_SCANCODE_W:
-          processor.toggle_key(0x5, true);
-          break;
-        case SDL_SCANCODE_E:
-          processor.toggle_key(0x6, true);
-          break;
-        case SDL_SCANCODE_R:
-          processor.toggle_key(0xD, true);
-          break;
-        case SDL_SCANCODE_A:
-          processor.toggle_key(0x7, true);
-          break;
-        case SDL_SCANCODE_S:
-          processor.toggle_key(0x8, true);
-          break;
-        case SDL_SCANCODE_D:
-          processor.toggle_key(0x9, true);
-          break;
-        case SDL_SCANCODE_F:
-          processor.toggle_key(0xE, true);
-          break;
-        case SDL_SCANCODE_Z:
-          processor.toggle_key(0xA, true);
-          break;
-        case SDL_SCANCODE_X:
-          processor.toggle_key(0x0, true);
-          break;
-        case SDL_SCANCODE_C:
-          processor.toggle_key(0xB, true);
-          break;
-        case SDL_SCANCODE_V:
-          processor.toggle_key(0xF, true);
-          break;
-        }
+        toggle_key(evt.key.keysym.scancode, true);
       }
       else if (evt.type==SDL_KEYUP) {
-        switch (evt.key.keysym.scancode) {
-        default:
-          break;
-        case SDL_SCANCODE_1:
-          processor.toggle_key(0x1, false);
-          break;
-        case SDL_SCANCODE_2:
-          processor.toggle_key(0x2, false);
-          break;
-        case SDL_SCANCODE_3:
-          processor.toggle_key(0x3, false);
-          break;
-        case SDL_SCANCODE_4:
-          processor.toggle_key(0xC, false);
-          break;
-        case SDL_SCANCODE_Q:
-          processor.toggle_key(0x4, false);
-          break;
-        case SDL_SCANCODE_W:
-          processor.toggle_key(0x5, false);
-          break;
-        case SDL_SCANCODE_E:
-          processor.toggle_key(0x6, false);
-          break;
-        case SDL_SCANCODE_R:
-          processor.toggle_key(0xD, false);
-          break;
-        case SDL_SCANCODE_A:
-          processor.toggle_key(0x7, false);
-          break;
-        case SDL_SCANCODE_S:
-          processor.toggle_key(0x8, false);
-          break;
-        case SDL_SCANCODE_D:
-          processor.toggle_key(0x9, false);
-          break;
-        case SDL_SCANCODE_F:
-          processor.toggle_key(0xE, false);
-          break;
-        case SDL_SCANCODE_Z:
-          processor.toggle_key(0xA, false);
-          break;
-        case SDL_SCANCODE_X:
-          processor.toggle_key(0x0, false);
-          break;
-        case SDL_SCANCODE_C:
-          processor.toggle_key(0xB, false);
-          break;
-        case SDL_SCANCODE_V:
-          processor.toggle_key(0xF, false);
-          break;
-        case SDL_SCANCODE_L:
+        if (evt.key.keysym.scancode==SDL_SCANCODE_L) {
           show_debug_log = !show_debug_log;
           SDL_LogSetPriority(SDL_LOG_CATEGORY_APPLICATION,
               show_debug_log ? SDL_LOG_PRIORITY_DEBUG : SDL_LOG_PRIORITY_INFO);
-          break;
+        }
+        else {
+          toggle_key(evt.key.keysym.scancode, false);
         }
       }
     }
